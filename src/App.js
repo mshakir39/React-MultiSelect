73b01e57000e6f25d56f608da8d8dd6fdc7c506e
Muzamil Shakir <mshakir39@gmail.com>
2022-12-18 19:21:38 +0500

    initail Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
+import Multi from "./Component/MulticheckDropdown";
+import React, { useState } from "react";
+
+const data = [
+  {
+    display: " All",
+    value: "",
+  },
+  {
+    display: " Apple",
+    value: "Apple",
+  },
+  { display: "Orange", value: "Orange" },
+];
+let value = "";
+const getSelectedValue = (childValue, placeholder) => {
+  console.log("value", childValue);
+  value = childValue;
+};
 function App() {
+  // const [status, setstatus] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {value}
+      <div className="" style={{ width: "" }}>
+        <Multi
+          Option={data}
+          getSelectedValue={getSelectedValue}
+          fontFamily={"monospace"}
+          IconColor={"rgb(100 37 84)"}
+        ></Multi>
+      </div>
     </div>
   );
 }
